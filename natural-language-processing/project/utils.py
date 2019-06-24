ee4a91c02ecb0ed53513216a0c756d56bac1ac16
Monique <moniquelouise@gmail.com>
2019-06-24 14:26:26 -0300

    initial version

---
+++ natural-language-processing/project/utils.py
@@ -0,0 +1,90 @@
+import nltk
+import pickle
+import re
+import numpy as np
+import joblib
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# Paths for all resources for the bot.
+RESOURCE_PATH = {
+    'INTENT_RECOGNIZER': 'intent_recognizer.pkl',
+    'TAG_CLASSIFIER': 'tag_classifier.pkl',
+    'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
+    'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
+    'WORD_EMBEDDINGS': 'word_embeddings.tsv',
+}
+
+
+def text_prepare(text):
+    """Performs tokenization and simple preprocessing."""
+    
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = bad_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+
+    return text.strip()
+
+
+def load_embeddings(embeddings_path):
+    """Loads pre-trained word embeddings from tsv file.
+
+    Args:
+      embeddings_path - path to the embeddings file.
+
+    Returns:
+      embeddings - dict mapping words to vectors;
+      embeddings_dim - dimension of the vectors.
+    """
+    
+    # Hint: you have already implemented a similar routine in the 3rd assignment.
+    # Note that here you also need to know the dimension of the loaded embeddings.
+    # When you load the embeddings, use numpy.float32 type as dtype
+
+    ########################
+    #### YOUR CODE HERE ####
+    ########################
+    dim = None
+    starspace_embeddings = dict()
+    for line in open(embeddings_path):
+        q, *ex = line.strip().split('\t')
+        dim = len(ex)
+        embedding = np.zeros(dim)
+        for i in range(dim):
+            embedding[i] = ex[i]
+        starspace_embeddings[q] = embedding
+
+    return starspace_embeddings, dim
+
+
+def question_to_vec(question, embeddings, dim):
+    """Transforms a string to an embedding by averaging word embeddings."""
+    
+    # Hint: you have already implemented exactly this function in the 3rd assignment.
+
+    ########################
+    #### YOUR CODE HERE ####
+    ########################
+    vec = np.zeros(dim)
+    words = question.split()
+    len = 0
+
+    for word in words:
+        if word in embeddings:
+            vec += embeddings[word]
+            len += 1
+
+    if len > 0:
+        return vec / len
+
+    return vec
+
+def unpickle_file(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
